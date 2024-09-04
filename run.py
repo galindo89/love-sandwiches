85fb2358f2a0811533d84273d7f25cb0cf9c41da
Pablo <pablo@livedooh.com>
2024-09-05 00:11:51 +0200

    adding functions to calculate stock and add them to the corresponding worksheet

--- run.py
+++ run.py
@@ -4,6 +4,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 from pprint import pprint
+import math
 SCOPE=[
 
     "https://www.googleapis.com/auth/spreadsheets",
@@ -101,17 +102,27 @@ def get_last_5_entries_sales():
     """
 
     sales=SHEET.worksheet("sales")
-    number_of_columns = sales.col_count()
-    
-    print(f"Number of columns: {number_of_columns}")
+    colums=[]
+    for ind in range(1,7):
+        column=sales.col_values(ind)
+        colums.append(column[-5:])
 
+    return colums
 
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%.
+    """
 
+    print("Calculating stock data...\n")
+    new_stock_data=[]
+    for column in data:
+        int_column=[int(num) for num in column]
+        average=sum(int_column)/len(int_column)
+        stock_num=average*1.1
+        new_stock_data.append(round(stock_num))
 
-
-
-
-    
+    return new_stock_data   
       
 
 def main():
@@ -124,12 +135,18 @@ def main():
     update_worksheet(sales_data, "sales")  
     new_surplus_data=calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_colums=get_last_5_entries_sales()
+    stock_data=calculate_stock_data(sales_colums)
+    update_worksheet(stock_data, "stock")   
+    print(stock_data)
 
 
 print("Welcome to Love Sandwiches Data Automation")
 
+main()
 
-get_last_5_entries_sales()
+
+
 
 
 
